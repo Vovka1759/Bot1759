41432dc4c8c2e2ee652e6647bf12f19376fdf05f
Volodymyr Stanskov <vovka1759@gmail.com>
2024-01-01 21:31:34 +0200

    3

--- index.js
+++ index.js
@@ -5,24 +5,28 @@ const TelegramBot = require("node-telegram-bot-api");
 const TikChan = require("tikchan");
 
 const token = process.env["BOT_TOKEN"];
+const instagramDl = require("@sasmeee/igdl");
 
 const bot = new TelegramBot(token, { polling: true });
 const dateStart = Date.now();
 
 const tiktokURLregx = /https?:\/\/?vm.tiktok.com\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
+const instaURLregx = /https?:\/\/?www.instagram.com\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
 
 console.log(`Bot started at ${dateStart}`);
 
 bot.on("message", (msg) => {
   if (msg.date * 1000 > dateStart) {
-    let linksArray = msg.text?.match(tiktokURLregx);
-    linksArray && linksArray.forEach(url => download(url, msg))
+    let linksArrayTt = msg.text?.match(tiktokURLregx);
+    linksArrayTt && linksArrayTt.forEach(url => downloadTt(url, msg))
+    let linksArrayInst = msg.text?.match(instaURLregx);
+    linksArrayInst && linksArrayInst.forEach(url => downloadInsta(url, msg))
   } else {
     console.log(`Message was at ${msg.date}, but bot started at ${dateStart}`);
   }
 });
 
-function download(url, msg) {
+function downloadTt(url, msg) {
   TikChan.download(url)
     .then((results) => {
       results["no_wm"] && 
@@ -31,4 +35,13 @@ function download(url, msg) {
     .catch((err) => {
       console.log(err);
     });
+}
+function downloadInsta(url, msg) {
+  instagramDl(url)
+  .then((results) => {
+    bot.sendMediaGroup(msg.chat.id, results.map((e) => { return {type: "video", "media" : e.download_link}; }));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }
